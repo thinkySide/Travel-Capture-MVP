865820edb6d9c1eef8b6e38157c6cabb943d580c
thinkyside <eunlyuing@gmail.com>
2025-03-28 15:55:17 +0900

    Xcode Project 생성

---
+++ Travel-Capture-MVP/Travel-Capture-MVP/Travel_Capture_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_Capture_MVPApp.swift
+//  Travel-Capture-MVP
+//
+//  Created by 김민준 on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Travel_Capture_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
